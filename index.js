70e967bc31617cc8358096c7844ef96675984472
Carlos Henrique Gaspari <carlos.gaspari@forlogic.net>
2024-04-28 22:13:45 -0300

    add env

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
+require('dotenv/config');
 const express = require('express');
-const mysql = require('mysql2');
-// Cria uma instância do Express
+const mysql2 = require('mysql2');
 const app = express();
 
-// Configuração da conexão com o banco de dados MySQL
-const connection = mysql.createConnection({
-  host: 'viaduct.proxy.rlwy.net',  // Host do seu banco de dados MySQL
-  user: 'root',  // Usuário do banco de dados
-  port: 49839,
-  password: 'uoLBLTvhVAlsdpXTejdfDcJVyWNRaNqJ',  // Senha do banco de dados
-  database: 'railway'  // Nome do banco de dados
+const connection = mysql2.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  port: process.env.MYSQL_PORT,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 });
 
-// Conecta-se ao banco de dados
 connection.connect((err) => {
   if (err) {
     console.error('Erro ao conectar ao banco de dados:', err);
@@ -21,12 +19,9 @@ connection.connect((err) => {
   console.log('Conexão ao banco de dados MySQL estabelecida com sucesso');
 });
 
-// Rota GET para buscar todos os usuários na tabela users
 app.get('/users', (req, res) => {
-  // Consulta SQL para selecionar todos os usuários
   const sql = 'SELECT * FROM users';
 
-  // Executa a consulta no banco de dados
   connection.query(sql, (err, results) => {
     if (err) {
       console.error('Erro ao executar a consulta:', err);
@@ -34,13 +29,11 @@ app.get('/users', (req, res) => {
       return;
     }
 
-    // Retorna os resultados da consulta como resposta
     res.json(results);
   });
 });
 
-// Define a porta em que o servidor Express irá escutar
-const port = 3000;  // Porta que deseja utilizar
+const port = 3000;
 app.listen(port, () => {
   console.log(`Servidor Express está ouvindo na porta ${port}`);
 });
