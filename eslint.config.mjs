90e4258a4e7f83e4f3875d3a08c9367e50223e64
Carlos Henrique Gaspari <carlos.gaspari@forlogic.net>
2024-04-28 22:30:23 -0300

    ajuste

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,15 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
-  {globals: {  process: true }},
+  { 
+    files: ["**/*.js"], 
+    languageOptions: { 
+      sourceType: "commonjs",
+      globals: {
+        process: true,
+        console: true 
+      }
+    }
+  },
   pluginJs.configs.recommended,
 ];
