6e7d2c3d040390330561a8a2656fab89af77beed
Carlos Henrique Gaspari <carlos.gaspari@forlogic.net>
2024-05-31 11:58:21 -0300

    teste build

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+//const jwt = require('jsonwebtoken');
 
 const app = express();
 
