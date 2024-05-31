3f81d7d5b57cda75630826f2b3fd7719ae5e0a4e
Carlos Henrique Gaspari <carlos.gaspari@forlogic.net>
2024-05-31 16:01:42 -0300

    teste build

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-// require('dotenv').config({ path: './config/.env' });
+require('dotenv').config({ path: './config/.env' });
 
 const express = require('express');
 const cors = require('cors');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(cors());
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
-// const userRoutes = require('./routes/userRoutes');
-// const productRoutes = require('./routes/productRoutes');
+const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
 
-// app.use('/users', userRoutes);
-// app.use('/products', productRoutes);
+app.use('/users', userRoutes);
+app.use('/products', productRoutes);
 
 app.get('/', (req, res) => {
   res.send('Bem-vindo à API demands!!');
